3ebf4bad39eb6d4bcaf31d95ae988cd44dffc3d2
Leo <leomrcpe@gmail.com>
2022-07-16 13:00:28 -0300

    Starting Project

---
+++ backend/src/main/java/com/cproject/svmeta/SvmetaApplication.java
@@ -0,0 +1,13 @@
+package com.cproject.svmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cproject/svmeta/SvmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cproject.svmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
